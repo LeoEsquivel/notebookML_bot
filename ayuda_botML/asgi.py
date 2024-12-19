bf544df965ceb314d31f447b69e0a5bb4c2ddbad
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-12-19 15:01:43 -0600

    Inicio proyecto

---
+++ ayuda_botML/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ayuda_botML project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ayuda_botML.settings')
+
+application = get_asgi_application()
