8854784660ea6777414e6ba93690be02d603250a
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-12-20 14:03:03 -0600

    UploadedDocument model register Admin panel

--- modelos/admin.py
+++ modelos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from modelos.models import UploadedDocument
+
+@admin.register(UploadedDocument)
+class UploadedDocumentAdmin(admin.ModelAdmin):
+    list_display       = ('id','title')
+    search_fields      = ['id','title']
+    list_display_links = ('id','title')
