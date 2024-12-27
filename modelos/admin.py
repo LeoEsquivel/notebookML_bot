66401cf0e46a554d00890efe6b70470508317e6c
Daniel Esquivel <danielesquivel00.lded@gmail.com>
2024-12-27 14:13:49 -0600

    Question model added to admin panel

--- modelos/admin.py
+++ modelos/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 
-from modelos.models import UploadedDocument
+from modelos.models import Question, UploadedDocument
 
 @admin.register(UploadedDocument)
 class UploadedDocumentAdmin(admin.ModelAdmin):
     list_display       = ('id','title')
     search_fields      = ['id','title']
     list_display_links = ('id','title')
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display       = ('id','question_text')
+    search_fields      = ['id','question_text']
+    list_display_links = ('id','question_text')
